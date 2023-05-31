353979b02ef1953bef6cee3023f70d84da82b82f
Shin-sibainu <shincode0712@gmail.com>
2023-05-31 15:23:39 +0900

    mod

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index
+    @posts = Post.all 
+    render json: @posts
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created 
+    else 
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post 
+    @post = Post.find(params[:id])
+  end
+end
